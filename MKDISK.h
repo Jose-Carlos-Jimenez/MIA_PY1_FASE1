c28a2e2c58c88683f479110bcb319d20ac4e706f
Jose-Carlos-Jimenez <2996381970101@ingenieria.usac.edu.gt>
2020-06-05 18:57:14 -0600

    [UPDATE] Agregando escritura de disk y su raid

--- MKDISK.h
+++ MKDISK.h
@@ -6,6 +6,12 @@
 #include <math.h>
 #include <string.h>
 #include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <libgen.h>
+#include <fstream>
+
+using namespace std;
 
 class MKDISK_
 {
@@ -15,6 +21,10 @@ private:
     std::string unit;
     char path[255];
     bool correct;
+    struct kbyte
+    {
+        char space[1024] = "0";
+    };
 
 public:
     MKDISK_(){};
@@ -25,6 +35,10 @@ public:
     void setCorrect();
     void printMk();
     std::string convertToString(char*,int);
+    int createDisk();
+    int getSize();
+    int getUnit();
+    std::string toLowerString(string);
 };
 
 void MKDISK_::setSize(char *value)
@@ -44,7 +58,18 @@ void MKDISK_::setUnit(char *value)
 
 void MKDISK_::setPath(char *value)
 {
-    strcpy(this->path, value);
+    if(value[0]=='\"')
+    {
+        string aux = value;
+        aux = aux.substr(1,aux.length()-2);
+        const char* nValue = aux.c_str();
+        strcpy(this->path, nValue);
+        cout << this->path << endl;
+    }
+    else
+    {
+        strcpy(this->path, value);
+    }
 }
 
 void MKDISK_::setCorrect()
@@ -52,7 +77,18 @@ void MKDISK_::setCorrect()
     this->correct = false;
     if(this->path[0] == '/' && this->size != 0)
     {
-        this->correct = true;
+        if(std::string(this->path) != "/" && size > 0)
+        {
+            this->correct = true;
+        }
+        else
+        {
+            cout << "Algunos de los parametros no cumplen con los requerimentos." << endl;
+        }
+    }
+    else
+    {
+        cout << "Algunos de los parametros no cumplen con los requerimentos." << endl;
     }
 }
 
@@ -62,6 +98,75 @@ void MKDISK_::printMk()
     printf("Fit: %s\n", this->fit_);
     printf("Size: %i\n", this->size);
     printf("Unidad: %s\n", this->unit.c_str());
+    createDisk();
+}
+
+int MKDISK_::createDisk()
+{
+    setCorrect();
+    if(this->correct)
+    {
+        string pathClone = this->path;
+        string pathTwice = this->path;
+        const size_t last_slash_idx = pathClone.find_last_of("/");
+        if (std::string::npos != last_slash_idx)
+        {
+            pathClone = pathClone.substr(0, last_slash_idx);
+        }
+        string comando = "mkdir -p \'";
+        comando+= dirname(path);
+        comando+= '\'';
+        system(comando.c_str());
+        FILE *f;
+        f = fopen(pathTwice.c_str(),"wb");
+        if(f == NULL)
+        {
+            return 0;
+        }
+        int sizeOfArray = getSize();
+        for(int i = 0; i < sizeOfArray; ++i)
+        {
+            char space[1024];
+            if(i==0)
+            {
+                space[0] = '0';
+            }
+            fwrite(&space, sizeof(space), 1, f);
+        }
+        fflush(f);
+        fclose(f);
+        ifstream  original_disk(pathTwice, std::ios::binary);
+        string name_without_ext = pathTwice.substr(0, pathTwice.size()-5);
+        ofstream  dst(name_without_ext+"[RAID].disk",   std::ios::binary);
+        dst << original_disk.rdbuf();
+
+        return 1;
+    }
+    return 0;
+}
+
+int MKDISK_::getSize()
+{
+    int tam = this->size*this->getUnit();
+    return tam;
+}
+
+int MKDISK_::getUnit()
+{
+    this->unit = this->toLowerString(this->unit);
+    if(this->unit=="m")
+    {
+        return 1024;
+    }
+    return 1;
+}
+
+string MKDISK_::toLowerString(string input)
+{
+    std::for_each(input.begin(), input.end(), [](char & c){
+        c = ::tolower(c);
+    });
+    return input;
 }
 
 std::string convertToString(char* a, int size)
